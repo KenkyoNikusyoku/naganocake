6c13a1c8726570d8bc524251fa3e1865406fdb0a
vagrant <vagrant@localhost.localdomain>
2020-06-02 02:08:11 +0000

    g-model-admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  #admin
+  devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #admin
+  namespace :admin do
+    resources :members,only: [:index, :show, :edit, :update, :top]
+    resources :products,only: [:index, :show, :new, :create, :edit,:update]
+    resources :gernrs,only: [:index, :create, :edit, :update]
+    resources :orders,only: [:index, :show, :update]
+  end
+
 end
