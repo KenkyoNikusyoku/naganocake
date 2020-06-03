7a27fb5cef1c6ea6591758730c418b53da066125
vagrant <vagrant@localhost.localdomain>
2020-06-03 07:51:37 +0000

    error

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,4 +1,9 @@
 class Admin::GenresController < ApplicationController
+    before_action :authenticate_admin!
+    #,edit,updateの内容重複の為
+    before_action :set_genre, only: [:edit, :update]
+
+    
     def index
         @genres = Genres.paginate(page:params[:page])
         @genre = Genre.new
@@ -14,6 +19,18 @@ class Admin::GenresController < ApplicationController
     end
 
     def update
+        @genre.update
+        redirect_to
+    end
+
+    private
+
+    def genre_params
+        params.require(genre).permit(:name, :is_valid)
+    end
+
+    def set_genre
+        @genre = Genre.find(params[:id])
     end
 
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -5,7 +5,8 @@ class Admin::ProductsController < ApplicationController
 
 
     def index
-        @products = Products.paginate(page:params[:page])
+        @products = Product.all
+        #Products.paginate(page:params[:page])
     end
 
     def edit
@@ -13,7 +14,7 @@ class Admin::ProductsController < ApplicationController
 
     def update
         @product.update(product_params)
-        redirect_to 
+        redirect_to admin_products_path
     end
 
     def show
@@ -26,13 +27,13 @@ class Admin::ProductsController < ApplicationController
     def create
         @product = Product.new(product_params)
         @product.save
-        redirect_to 
+        redirect_to admin_products_path
     end
 
     private
 
     def product_params
-        params.require(:product).permit(:name, :introduction, :image, :price, :is_valid)
+        params.require(:product).permit(:name, :introduction, :price)
     end
 
     def set_product
