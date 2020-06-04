7152dbb06debb9ac66718cc29a3e5321c965d178
mimimisaki <mimimisaki2@gmail.com>
2020-06-04 16:36:25 +0900

    second commit

--- app/controllers/member/members_controller.rb
+++ app/controllers/member/members_controller.rb
@@ -11,18 +11,22 @@ class Member::MembersController < ApplicationController
 	end
 
 	def update
+		@member = Member.find(params[:id])
+		if @member.update(member_params)
+			redirect_to member_path(@member)
+		else
+		end
+	end
+
+	def quit
+		@member = Member.find(params[:id])
 	end
 
 	def hide
 	end
 
-
-
-
-
-	#private
-
-    #def user_params
-      #params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    #end
+	private
+    def member_params
+    	params.require(:member).permit(:member_id, :first_name, :last_name, :first_name_kana, :last_name_kana, :email, :telephone, :postal_code, :address, :is_deleted)
+    end
 end
