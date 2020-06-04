f691dbcbac8cf5729184bb9815c2fb111ec0a730
kyoto93 <k12a24r@gmail.com>
2020-06-04 07:36:39 +0000

    cartproduct_controller編集　途中

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,20 @@ class Product < ApplicationRecord
 	belongs_to :genre
 	has_many :cart_products, dependent: :destroy
 	has_many :order_products
+
+	attacment :image
+    #destroy:　false　必要？
+
+    validates :name, presence: true
+    #商品名
+    #presence: true は存在しているかどうか
+
+    validates :price, presence: true
+    #税抜き価格
+
+    validates :is_valid, inclusion: { in: [true, false] }#合ってる？
+    #販売ステータス
+
+    #参考記事
+    #https://qiita.com/h1kita/items/772b81a1cc066e67930e
 end
