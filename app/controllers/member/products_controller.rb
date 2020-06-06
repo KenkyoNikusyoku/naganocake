95c3cf87a2135f54084a9e0fc94a2dfb78cf844e
kyoto93 <k12a24r@gmail.com>
2020-06-06 02:52:02 +0000

    cartproduct-productまだ途中

--- app/controllers/member/products_controller.rb
+++ app/controllers/member/products_controller.rb
@@ -3,10 +3,12 @@ class Member::ProductsController < ApplicationController
 
 	def index
         @products = Product.all
+        @genres = Genre.all
     end
 
     def show
          @product = Product.find(params[:id])
          @cart_product = CartProduct.new
+         @genres = Genre.all
     end
 end
