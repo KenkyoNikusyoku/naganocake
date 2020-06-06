687ac1d5de40bc5aa74ae6d8fc86d5872d6d7566
mimimisaki <mimimisaki2@gmail.com>
2020-06-06 18:48:37 +0900

    admin product 編集

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -14,8 +14,11 @@ class Admin::ProductsController < ApplicationController
     end
 
     def update
-        @product.update(product_params)
-        redirect_to admin_products_path
+        if @product.update(product_params)
+            redirect_to admin_products_path
+        else
+            render 'edit'
+        end
     end
 
     def show
@@ -24,20 +27,23 @@ class Admin::ProductsController < ApplicationController
     def new
         @product = Product.new
       end
-    
-      def create
+
+    def create
         @product = Product.new(product_params)
-        @product.save
-        redirect_to admin_products_path
-        
-      end
+        if @product.save
+            redirect_to admin_products_path
+        else
+            render 'new'
+        end
+
+    end
 
     private
 
     def product_params
         params.require(:product).permit(:name, :introduction, :price, :genre_id, :is_valid, :image)
     end
-    
+
     def set_product
         @product = Product.find(params[:id])
     end 
