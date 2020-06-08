da8a773b633452335024e45dc6de1daa1516ce99
vagrant <vagrant@localhost.localdomain>
2020-06-08 06:51:32 +0000

    cart-product-controller

--- app/controllers/member/cart_products_controller.rb
+++ app/controllers/member/cart_products_controller.rb
@@ -8,7 +8,7 @@ class Member::CartProductsController < ApplicationController
     def update
         @cart_products = CartProduct.find(params[:id])
         if @cart_products.update(cart_product_params)
-          redirect_to cart_products_path,success: '個数を変更しました'
+          redirect_to cart_products_path,notice: '個数を変更しました'
         else
           render :index, danger: "個数の変更に失敗しました。"
         end
@@ -18,7 +18,7 @@ class Member::CartProductsController < ApplicationController
         @cart_product = CartProduct.new(cart_product_params)
         @cart_product.member_id = current_member.id
         if @cart_product.save
-          redirect_to cart_products_path,success: '商品をカートに追加しました.'
+          redirect_to cart_products_path,notice: '商品をカートに追加しました.'
         else
           @product = Product.find(@cart_product.product.id)
           render 'products/show'
@@ -30,13 +30,13 @@ class Member::CartProductsController < ApplicationController
     def destroy
         cart_product = CartProduct.find(params[:id])
         cart_product.destroy
-        redirect_to cart_products_path,success: '商品の削除が完了しました。'
+        redirect_to cart_products_path,notice: '商品の削除が完了しました。'
     end
 
     def all_destroy
         member = Member.find(current_member.id)
         if member.cart_products.destroy_all
-          redirect_to cart_products_path,success: 'カート内の商品を全て削除しました。'
+          redirect_to cart_products_path,notice: 'カート内の商品を全て削除しました。'
         else
           render :index, danger: "カート内の商品を削除出来ませんでした。"
         end
