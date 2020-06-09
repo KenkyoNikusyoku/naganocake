4bd5d6089588ad82aa4325110208c30eb07c7142
vagrant <vagrant@localhost.localdomain>
2020-06-09 07:17:40 +0000

    fix

--- app/models/cart_product.rb
+++ app/models/cart_product.rb
@@ -1,7 +1,10 @@
 class CartProduct < ApplicationRecord
-	with_options presence: true do
-        validates :member_id
+
+	belongs_to :member
+    belongs_to :product
+    with_options presence: true do
+        validates :user_id
         validates :product_id
         validates :quantity
-    end
+      end
 end
